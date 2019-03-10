9c5f0380964ba884c1f406305a761d9bf1acdd59
Nahid Hasan <rajunahidhasan0@gmail.com>
2019-03-10 12:58:37 +0600

    Basic structure for our project

---
+++ GetServices2/app/src/main/java/com/example/getservices/Chottogram.java
@@ -0,0 +1,23 @@
+package com.example.getservices;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Chottogram<buttonEle> extends AppCompatActivity {
+
+
+    Button buttonEle, buttonBri, buttonPlu, buttonHom, buttonDes, buttonLib, buttonGar, buttonSof,
+            buttonDen, buttonDoc, buttonPol, buttonLab, buttonFar, buttonBro, buttonEme;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chottogram);
+
+    }
+
+}
---
+++ GetServices2/app/src/main/java/com/example/getservices/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.getservices;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonRaj, buttonRan, buttonDha, buttonSyl, buttonKhu, buttonBar, buttonCho, buttonMym;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
