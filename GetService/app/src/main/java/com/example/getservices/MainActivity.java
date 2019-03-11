6843843ec6dfeaae8cfe5e74ad75b0839d4f0be1
Nahid Hasan <37169055+rajunahidhasan0@users.noreply.github.com>
2019-03-11 15:41:43 +0600

    Modify Main Activity
    Modify Main Activity with respect to the change done by other branch

--- GetService/app/src/main/java/com/example/getservices/MainActivity.java
+++ GetService/app/src/main/java/com/example/getservices/MainActivity.java
@@ -16,5 +16,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Hide all the titlebar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        buttonRaj = (Button) findViewById(R.id.Rajshahi);
+
+        buttonRaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Rajshahi.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "রাজশাহীতে আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonRan = (Button) findViewById(R.id.Rangpur) ;
+        buttonRan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Rangpur.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "রংপুরে আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonDha = (Button) findViewById(R.id.Dhaka) ;
+        buttonDha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Dhaka.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "ঢাকায় আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonSyl = (Button) findViewById(R.id.Sylhet) ;
+        buttonSyl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Sylhet.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "িলেটে আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonBar = (Button) findViewById(R.id.Barishal) ;
+        buttonBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Barishal.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "বরিশালে আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonKhu = (Button) findViewById(R.id.Khulna) ;
+        buttonKhu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Khulna.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "খুলনা আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonCho = (Button) findViewById(R.id.Chottogram) ;
+        buttonCho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Chottogram.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "চট্টগ্রাম আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonMym = (Button) findViewById(R.id.Mymensingh) ;
+        buttonMym.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Mymensingh.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "ময়মনসিংহে আপনি যেসকল সেবা পেতে পারেন", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
