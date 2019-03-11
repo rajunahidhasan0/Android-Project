92103e2f746213e18f654b916f0aaf9eeaff78e9
iftekher148 <37367596+iftekher148@users.noreply.github.com>
2019-03-11 15:32:45 +0600

    Added Second activities into Main Activity

--- GetService/app/src/main/java/com/example/getservices/Rajshahi.java
+++ GetService/app/src/main/java/com/example/getservices/Rajshahi.java
@@ -18,7 +18,154 @@ public class Rajshahi<buttonEle> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rajshahi);
 
+        buttonEle = (Button) findViewById(R.id.Electrician);
+        buttonEle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Electrician.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        buttonBri = (Button) findViewById(R.id.Bricklayer);
+        buttonBri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, BricksLayer.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonPlu = (Button) findViewById(R.id.Plumber);
+        buttonPlu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Plumber.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonHom = (Button) findViewById(R.id.HomeTutor);
+        buttonHom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, HomeTutor.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonDes = (Button) findViewById(R.id.Designer);
+        buttonDes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Designer.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonLib = (Button) findViewById(R.id.Librarian);
+        buttonLib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Librarian.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonGar = (Button) findViewById(R.id.Gardener);
+        buttonGar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Gardener.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonSof = (Button) findViewById(R.id.SoftwareDeveloper);
+        buttonSof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, SoftwareDeveloper.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonDen = (Button) findViewById(R.id.Dentist);
+        buttonDen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Dentist.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonDoc = (Button) findViewById(R.id.Doctor);
+        buttonDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Doctor.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonPol = (Button) findViewById(R.id.Police);
+        buttonPol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Police.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonLab = (Button) findViewById(R.id.Labour);
+        buttonLab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Labour.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonFar = (Button) findViewById(R.id.Farmer);
+        buttonFar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Farmer.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonBro = (Button) findViewById(R.id.Broker);
+        buttonBro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Broker.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonEme = (Button) findViewById(R.id.Emergency);
+        buttonEme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Rajshahi.this, Emergency.class);
+                startActivity(intent);
+                Toast.makeText(Rajshahi.this, "আপনার পছন্দমতো সেবা প্রদানকারীর সাথে যোগাযোগ করুন", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
