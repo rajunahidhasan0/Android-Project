e11877f98f61785a3bfc8306b367b58cf8642281
Md. Abdul Mozid <csemozid@gmail.com>
2019-03-11 23:57:12 +0600

    Call and Mail Buttons are added.

---
+++ GetService/app/src/main/res/layout/buttonMail2.java
@@ -0,0 +1,13 @@
+package com.example.getservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class buttonMail2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_mail2);
+    }
+}
